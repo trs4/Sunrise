a76656ce2e9d57ae568813eba6ad31dc51d0b539
Алексей Михеев <amikheev45@yandex.ru>
2026-01-06 19:05:28 +0300

    device Track_PointerMoved

--- Sunrise/Views/TrackPlayDeviceView.axaml.cs
+++ Sunrise/Views/TrackPlayDeviceView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Sunrise.ViewModels;
 
 namespace Sunrise.Views;
@@ -12,16 +13,29 @@ public partial class TrackPlayDeviceView : UserControl
 
     private void Track_Tapped(object? sender, TappedEventArgs e)
     {
-        if (DataContext is not TrackPlayViewModel viewModel || ((e.Source as ContentPresenter)?.Content as ContentPresenter)?.Content is Image)
-            return;
-
-        var currentTrack = viewModel.CurrentTrack;
-
-        if (currentTrack is null)
+        if (!TryGetCurrentTrack(e, out var viewModel, out _))
             return;
 
         double position = e.GetPosition(progressBar).X / progressBar.Bounds.Width;
-        viewModel.ChangePosition(position);
+        viewModel!.ChangePosition(position);
+    }
+
+    private void Track_PointerMoved(object? sender, PointerEventArgs e)
+    {
+        var point = e.GetCurrentPoint(sender as Control);
+
+        if (!point.Properties.IsLeftButtonPressed || !TryGetCurrentTrack(e, out var viewModel, out _))
+            return;
+
+        double position = e.GetPosition(progressBar).X / progressBar.Bounds.Width;
+        viewModel!.ChangePosition(position);
+    }
+
+    private bool TryGetCurrentTrack(RoutedEventArgs e, out TrackPlayViewModel? viewModel, out TrackViewModel? currentTrack)
+    {
+        viewModel = DataContext as TrackPlayViewModel;
+        currentTrack = viewModel?.CurrentTrack;
+        return viewModel is not null && currentTrack is not null && ((e.Source as ContentPresenter)?.Content as ContentPresenter)?.Content is not Image;
     }
 
     private async void TrackTitle_Tapped(object? sender, TappedEventArgs e)
