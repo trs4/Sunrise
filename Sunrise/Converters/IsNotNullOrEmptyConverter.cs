cb244f915fa1fc91170c7b1e83cfa30674e5e985
Алексей Михеев <amikheev45@yandex.ru>
2026-01-19 20:59:14 +0300

    fix margin on tabs

---
+++ Sunrise/Converters/IsNotNullOrEmptyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Sunrise.Converters;
+
+public sealed class IsNotNullOrEmptyConverter : IValueConverter
+{
+    public static readonly IsNotNullOrEmptyConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is string str && str.Length > 0;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
